ad910ee000e6d1c31e7aeb6e7195a19919983544
Kera-ab <128666578+Kera-ab@users.noreply.github.com>
2024-05-13 19:59:02 +0800

    save

--- src/Mainpage/Home.jsx
+++ src/Mainpage/Home.jsx
@@ -7,8 +7,8 @@ function Home() {
     <div className='Page'>
       <div className='Text-Area'>
         <br/>
-        <h1>Swift Sync Services </h1>
-        <h2>Providing What You Need</h2>
+        <h2>Dream Big Studios NP</h2>
+        <p>Providing What You Need</p>
         <p className='mainbody'>SwiftSync Services offers a wide range of consulting services with necessary tools and expertise to help grow your business. We partner with our clients from start to finish,focusing on  thier needs while producing new ideas, developing effective strategies and designing high quality and scalable solutions. Contact us to learn more.</p>
       </div>
     </div>
